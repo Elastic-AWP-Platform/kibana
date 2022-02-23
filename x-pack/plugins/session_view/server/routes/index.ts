cf183b3e97ca9b7b6117230634f519bf211cf4a2
Zizhou Wang <zizhou.wang@elastic.co>
2022-02-22 19:59:32 -0500

    Remove recent session route

--- x-pack/plugins/session_view/server/routes/index.ts
+++ x-pack/plugins/session_view/server/routes/index.ts
@@ -7,11 +7,9 @@
 import type { Logger } from 'kibana/server';
 import { IRouter } from '../../../../../src/core/server';
 import { registerProcessEventsRoute } from './process_events_route';
-import { registerRecentSessionRoute } from './recent_session_route';
 import { sessionEntryLeadersRoute } from './session_entry_leaders_route';
 
 export const registerRoutes = (router: IRouter, logger: Logger) => {
   registerProcessEventsRoute(router, logger);
-  registerRecentSessionRoute(router);
   sessionEntryLeadersRoute(router);
 };
